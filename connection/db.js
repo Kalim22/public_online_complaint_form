0a659d4271b44a1ef673f72ed339728ceca07ec7
kalim22 <kalimali2295@gmail.com>
2023-03-01 13:14:20 +0530

    changes in server file

--- connection/db.js
+++ connection/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-require("dotenv").config({
-  path: "./env/.env",
-});
+// require("dotenv").config({
+//   path: "./env/.env",
+// });
 
 mongoose.connect("mongodb://127.0.0.1:27017/", (err) => {
   if (err) {
--- server.js
+++ server.js
@@ -19,10 +19,9 @@ app.use("/", require("./routes/Authentication"));
 
 app.use("/", require("./routes/Profile"));
 
-const port = process.env.PORT || 8000;
-app.listen(port, (err) => {
+app.listen("8000", (err) => {
   if (err) {
     console.log(err);
   }
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running...`);
 });
